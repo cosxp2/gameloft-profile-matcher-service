6014716bfb8de23794c906edb9363ac29b578c73
Cosmin Pricop <cosminxpricop@gmail.com>
2025-08-07 00:09:18 +0200

    Update load player script

--- scripts/load_test_player.py
+++ scripts/load_test_player.py
@@ -1,22 +1,10 @@
-import json
 from datetime import datetime, timezone
-from app.adapters.db.sqlalchemy_models import PlayerProfileDBModel, Base
-from app.adapters.dependencies import engine, SessionLocal
-from sqlalchemy.orm import Session
-
-Base.metadata.create_all(bind=engine)
-
-def seed():
-    db: Session = SessionLocal()
-
-    from datetime import datetime, timezone
-import json
 from app.adapters.db.sqlalchemy_models import PlayerProfileDBModel
-from sqlalchemy.orm import Session
+from app.adapters.dependencies import SessionLocal
 
-def seed_test_player(db: Session):
-    dummy_date = datetime(2022, 1, 1, 0, 0, tzinfo=timezone.utc)
-    
+def seed_test_player():
+    dummy_date = datetime(2022, 1, 1, tzinfo=timezone.utc)
+
     test_player = PlayerProfileDBModel(
         player_id="test_player",
         credential="apple",
@@ -27,15 +15,13 @@ def seed_test_player(db: Session):
         total_refund=0.0,
         total_transactions=3,
         last_purchase=dummy_date,
-        active_campaigns=json.dumps([]),
-        devices=json.dumps([
-            {
-                "id": 1,
-                "model": "iPhone",
-                "carrier": "vodafone",
-                "firmware": "1.0"
-            }
-        ]),
+        active_campaigns=[],
+        devices=[{
+            "id": 1,
+            "model": "iPhone",
+            "carrier": "vodafone",
+            "firmware": "1.0"
+        }],
         level=5,
         xp=1000,
         total_playtime=80.0,
@@ -43,26 +29,24 @@ def seed_test_player(db: Session):
         language="en",
         birth_date=dummy_date,
         gender="male",
-        inventory=json.dumps({
+        inventory={
             "cash": 100.0,
             "coins": 50,
             "items": {
                 "item_1": 1
             }
-        }),
-        clan=json.dumps({
+        },
+        clan={
             "id": 1,
             "name": "test clan"
-        }),
+        },
         custom_field="demo"
     )
 
-    db.add(test_player)
-    db.commit()
-
-    db.add(test_player)
-    db.commit()
-    db.close()
+    with SessionLocal() as db:
+        db.merge(test_player)  
+        db.commit()
+        print("Test player seeded")
 
 if __name__ == "__main__":
-    seed()
+    seed_test_player()
